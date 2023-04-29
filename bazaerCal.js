3ddadad392ab05b1bacea9250f3ef6448acb4395
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-29 15:36:22 +0600

    Bazar cal in JavaScript

---
+++ bazaerCal.js
@@ -0,0 +1,5 @@
+let totalMoney = prompt("Enter total Money$: ");
+let spend = prompt("Enter the money you spend: ");
+let leftMoney = totalMoney - spend;
+
+document.write("You left only: " + leftMoney + " Taka"); 
